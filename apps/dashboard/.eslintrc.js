4cdd5afd5d5714592b934e42a25753e36ab4eb52
Jonas Daniels <jonas.daniels@outlook.com>
2024-04-30 10:57:34 -0700

    style: update background color and text color in globals.css

--- apps/dashboard/.eslintrc.js
+++ apps/dashboard/.eslintrc.js
@@ -186,12 +186,6 @@ module.exports = {
   },
   overrides: [
     {
-      files: "src/app/**/*",
-      rules: {
-        "react/forbid-dom-props": ["off", { forbid: ["className"] }],
-      },
-    },
-    {
       files: "src/core-ui/**/*",
       rules: {
         // no restricted imports
